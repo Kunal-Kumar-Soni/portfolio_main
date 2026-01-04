fa948189af6fe7b1099c3d98877297204b8a2cd7
Kunal <kunal.codes24@gmail.com>
2026-01-04 19:18:04 +0530

    github activity section improve

--- components/home/github-section.tsx
+++ components/home/github-section.tsx
@@ -24,13 +24,22 @@ const GithubActivity = () => {
         <h1 className="font-ibmPlexSans font-bold text-3xl">GitHub Activity</h1>
       </div>
 
-      <div className="bg-background px-5.5 py-5 border border-border rounded-xl">
+      <div className="bg-background py-2">
         <GitHubCalendar
           username="Kunal-Kumar-Soni"
           colorScheme={theme === "dark" ? "dark" : "light"}
           blockSize={10}
-          blockMargin={3}
+          blockMargin={3.9}
+          blockRadius={0}
           fontSize={12}
+          labels={{
+            totalCount: `{{count}} contributions in 2025`,
+          }}
+          theme={{
+            light: ["#ebedf0", "#afb5bb", "#6e7681", "#30363d", "#161b22"],
+            dark: ["#161b22", "#30363d", "#6e7681", "#bcbcbc", "#ffffff"],
+          }}
+          tooltips={{}}
         />
       </div>
     </div>
