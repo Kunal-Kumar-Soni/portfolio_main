91b77aff8d707dcb12cf6edc32c3985accfbc869
Kunal <kunal.codes24@gmail.com>
2026-01-06 18:53:22 +0530

    footer section change

--- components/main/footer.tsx
+++ components/main/footer.tsx
@@ -6,9 +6,31 @@ export default function Footer() {
   return (
     <footer className="bg-background mx-auto px-4 pt-10 pb-30 w-full max-w-3xl font-geistMono">
       <Separator />
+
       <p className="mt-4 text-muted-foreground text-sm text-center">
         © {currentYear} <span className="font-medium text-foreground">Kunal Kumar Soni</span>. All
         rights reserved.
+      </p>
+
+      <p className="mt-2 text-muted-foreground text-sm text-center">
+        Inspired by{" "}
+        <a
+          href="https://ramx.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-foreground underline transition"
+        >
+          Ram
+        </a>{" "}
+        &{" "}
+        <a
+          href="https://chanhdai.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-foreground underline transition"
+        >
+          Chánh Đại
+        </a>
       </p>
     </footer>
   );
