541dd685303248da782904db391227cba8b3ba1c
Kunal <kunal.codes24@gmail.com>
2026-01-15 16:18:25 +0530

    change projects card image type

--- data/projects-info.ts
+++ data/projects-info.ts
@@ -4,7 +4,7 @@ export const projectsInfo = [
     title: "Resume AI",
     description:
       "An AI-powered resume builder that analyzes content, improves clarity, and provides ATS-friendly scoring with real-time feedback.",
-    image: "/projectsImg/resume_ai.png",
+    image: "/projectsImg/resumeai1.png",
     status: "live",
     technologies: ["nextjs", "typescript", "tailwind", "supabase", "gemini", "shadcn"],
 
@@ -26,7 +26,7 @@ export const projectsInfo = [
     description:
       "A scalable e-commerce application featuring product browsing, cart state management with Redux, responsive design, and optimized UI interactions for a smooth user shopping experience.",
 
-    image: "/projectsImg/shop_karo.png",
+    image: "/projectsImg/shopkaro2.png",
     status: "building",
     technologies: ["react", "javascript", "tailwind", "redux"],
     SocialLinks: [
