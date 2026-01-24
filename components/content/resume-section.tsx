1df0f374ef26af8ad06208a4b4d42eb7c0ff50af
Kunal <kunal.codes24@gmail.com>
2026-01-24 18:37:12 +0530

    release: deploy to production

--- components/content/resume-section.tsx
+++ components/content/resume-section.tsx
@@ -1,43 +1,47 @@
-"use client";
 import { Download, ExternalLink } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 
 const ResumeSection = () => {
   return (
     <section className="mx-auto px-4 max-w-3xl">
-      {/* Header */}
-      <div className="flex sm:flex-row flex-col justify-between items-start sm:items-center gap-3 mb-6">
-        <div>
-          <h1 className="font-semibold text-2xl sm:text-3xl">Resume</h1>
-          <p className="mt-1 text-muted-foreground text-sm">View or download my resume</p>
-        </div>
 
-        {/* Actions */}
-        <div className="flex gap-3">
-          <Button asChild>
-            <Link
-              href="https://drive.google.com/file/d/14noj-TB1F7P8fyFwTVHOekVVRPcLQskq/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <ExternalLink size={16} />
-              Open
-            </Link>
-          </Button>
+      <div className="mb-10">
+        <h1 className="font-ibmPlexSans font-semibold text-4xl sm:text-5xl text-center tracking-tight">
+          Resume
+        </h1>
+        <p className="mt-4 text-muted-foreground text-lg text-center">
+          View my resume to explore my skills, experience, and projects.
+        </p>
+      </div>
+      <Separator className='my-10' />
 
-          <Button variant={"secondary"} asChild>
-            <Link
-              href="/documents/Kunal's_Resume.pdf"
-              download
-            >
-              <Download size={16} />
-              Download
-            </Link>
-          </Button>
-        </div>
+      {/* Buttons */}
+      <div className="flex justify-end items-end gap-3 mb-6">
+        <Button asChild>
+          <Link
+            href="https://drive.google.com/file/d/14noj-TB1F7P8fyFwTVHOekVVRPcLQskq/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ExternalLink size={16} />
+            Open
+          </Link>
+        </Button>
+
+        <Button variant={"secondary"} asChild>
+          <Link
+            href="/documents/Kunal's_Resume.pdf"
+            download
+          >
+            <Download size={16} />
+            Download
+          </Link>
+        </Button>
       </div>
 
+      {/* Resume PDF */}
       <div className="relative bg-background shadow-sm border">
         <div>
           <iframe
@@ -47,6 +51,7 @@ const ResumeSection = () => {
           />
         </div>
       </div>
+
     </section>
   );
 };
