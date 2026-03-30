5f824ff7376758e57ec853bea1300702a91926b7
Kunal <kunal.codes24@gmail.com>
2026-03-30 09:40:18 +0530

    chore: add prettier and linting configurations
    - Updated package.json to include formatting and linting scripts.
    - Added husky and lint-staged for pre-commit hooks.
    - Created .prettierignore to exclude unnecessary files from formatting.
    - Added .prettierrc.json for prettier configuration with plugins for sorting imports and tailwindcss.

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import Image from "next/image";
 import { BsFillFileTextFill, BsPatchCheckFill } from "react-icons/bs";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
 import { IoIosSend } from "react-icons/io";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Separator } from "@/components/ui/separator";
 import TechBadge from "@/components/ui/tech-badge";
+
+import { Button } from "../ui/button";
 import { LinksIcon } from "../ui/link-icon";
 
 export default function HeroSection() {
   const router = useRouter();
 
   return (
-    <section className="space-y-6 mx-auto px-4 max-w-3xl">
+    <section className="mx-auto max-w-3xl space-y-6 px-4">
       <div className="flex items-center gap-5">
         <div className="relative">
           {/* Profile */}
@@ -29,21 +32,25 @@ export default function HeroSection() {
 
         {/* Name */}
         <div>
-          <h1 className="flex items-center gap-2 font-semibold text-xl sm:text-2xl">
+          <h1 className="flex items-center gap-2 text-xl font-semibold sm:text-2xl">
             Kunal Kumar Soni
             <BsPatchCheckFill size={20} className="text-sky-400" />
           </h1>
 
-          <p className="text-muted-foreground md:text-base">23' Engineer</p>
+          <p className="text-muted-foreground md:text-base">
+            23&apos; Engineer
+          </p>
         </div>
       </div>
 
       <Separator />
 
-      <p className="text-muted-foreground text-lg leading-9 tracking-wide line">
-        Frontend developer working with <TechBadge tech="react" />, <TechBadge tech="nextjs" />, and{" "}
-        <TechBadge tech="typescript" /> to build modern, responsive web applications, using{" "}
-        <TechBadge tech="tailwind" /> and <TechBadge tech="shadcn" /> for clean and consistent UI.
+      <p className="text-muted-foreground line text-lg leading-9 tracking-wide">
+        Frontend developer working with <TechBadge tech="react" />,{" "}
+        <TechBadge tech="nextjs" />, and <TechBadge tech="typescript" /> to
+        build modern, responsive web applications, using{" "}
+        <TechBadge tech="tailwind" /> and <TechBadge tech="shadcn" /> for clean
+        and consistent UI.
       </p>
 
       <div>
@@ -53,9 +60,12 @@ export default function HeroSection() {
       {/* Buttons */}
       <div className="flex gap-4">
         {/* Resume Button */}
-        <Button onClick={() => router.push("/resume")} className="group cursor-pointer">
+        <Button
+          onClick={() => router.push("/resume")}
+          className="group cursor-pointer"
+        >
           <div className="flex items-center">
-            <BsFillFileTextFill className="text-base group-hover:-rotate-20 transition-transform duration-300" />
+            <BsFillFileTextFill className="text-base transition-transform duration-300 group-hover:-rotate-20" />
             <span className="ml-2">Resume / CV</span>
           </div>
         </Button>
@@ -68,7 +78,7 @@ export default function HeroSection() {
         >
           <div className="flex items-center">
             <span className="mr-2">Get in touch</span>
-            <IoIosSend className="text-base group-hover:rotate-45 transition-transform duration-300" />
+            <IoIosSend className="text-base transition-transform duration-300 group-hover:rotate-45" />
           </div>
         </Button>
       </div>
